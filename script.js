fda187ebc91b2ec01f408c89ab458076fccb9c70
Smit Mistry <84773605+smit-lab@users.noreply.github.com>
2023-12-26 14:15:47 +0530

    src change from "/" to "./"

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const answers = document.querySelectorAll(".answer");
 plusBtn.forEach((btn, index) => {
   btn.addEventListener("click", (e) => {
     if (e.target.src.includes("icon-plus")) {
-      e.target.src = "/images/icon-minus.svg";
+      e.target.src = "./images/icon-minus.svg";
     } else {
-      e.target.src = "/images/icon-plus.svg";
+      e.target.src = "./images/icon-plus.svg";
     }
     answers[index].classList.toggle("answer-show");
     answers.forEach((answer, i) => {
       if (i !== index) {
         answer.classList.remove("answer-show");
-        plusBtn[i].src = "/images/icon-plus.svg";
+        plusBtn[i].src = "./images/icon-plus.svg";
       }
     });
   });
